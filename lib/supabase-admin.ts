3814b37085f68e028ca71e7613250308f4afb7f8
ghyorka <ghyorkagospel@gmail.com>
2026-02-01 14:10:23 +0100

    fix: remove use server directive and make the help async to resolve build error

--- lib/supabase-admin.ts
+++ lib/supabase-admin.ts
@@ -1,8 +1,6 @@
-"use server";
-
 import { createClient } from "@supabase/supabase-js";
 
-export const createSupabaseAdminClient = () => {
+export const createSupabaseAdminClient = async () => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!, // Never expose publicly
